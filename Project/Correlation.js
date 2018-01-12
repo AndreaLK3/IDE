f2a71bd715a489e0fec8440373a0b9550de0e8f4
Jianfan Jeffrey Chen <Jeffrey@appledeMacBook-Pro-6.local>
2018-01-12 13:14:26 +0100

    sss

--- Project/Correlation.js
+++ Project/Correlation.js
@@ -1,78 +1,69 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+function drawScatterplot(variable1, variable2, width, height, data){
 
-/*
- * value accessor - returns the value to encode for a given data object.
- * scale - maps value to a visual display encoding, such as a pixel position.
- * map function - maps from data value to display value
- * axis - sets up axis
- */
+    var padding = 40;
 
-// setup x
-var xValue = function(d) { return d.Timestamp;}, // data -> value
-    xScale = d3.scaleLinear().range([0, width]), // value -> display
-    xMap = function(d) { return xScale(xValue(d));}, // data -> display
-    xAxis = d3.axisBottom().scale(xScale);
-
-// setup y
-var yValue = function(d) { return d.CO;}, // data -> value
-    yScale = d3.scaleLinear().range([height, 0]), // value -> display
-    yMap = function(d) { return yScale(yValue(d));}, // data -> display
-    yAxis = d3.axisLeft().scale(yScale);
-
-// setup fill color
-    // var cValue = function(d) { return d.Manufacturer;},
-     color = d3.scaleOrdinal("schemeCategory10");
-
-// add the graph canvas to the body of the webpage
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// add the tooltip area to the webpage
-var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-// load data
-d3.csv("AndersenBoulevard.csv", function(error, data) {
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g");
 
     // change string (from CSV) into number format
-    data.forEach(function(d) {
-        d.CO = +d.CO;
-    console.log(d);
+    data.forEach(function (d) {
+        //console.log(d[variable2]);
+        d[variable2] = +d[variable2];
+        d[variable1] = +d[variable1];
+        //console.log(d);
     });
 
-    // don't want dots overlapping axis, so add in buffer to data domain
-    // xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
-    // yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+    // setup x
+    var xValue = function (d) {return d[variable1];} // data -> value
+    console.log("Width: " + width)
+    console.log(0 + padding)
+    console.log( width - padding)
+    var xScale = d3.scaleLinear().range([0 + padding, width - padding]), // value -> display
+        xMap = function (d){
+            //console.log(xValue(d))
+            return xScale(d[variable1]);
+        }, // data -> display
+        xAxis = d3.axisBottom().scale(xScale);
+
+    // setup y
+    var yValue = function (d) {
+            return d[variable2];
+        }, // data -> value
+        yScale = d3.scaleLinear().range([height - padding, 0 + padding]), // value -> display
+        yMap = function (d) {
+            return yScale(d[variable2]);
+        }, // data -> display
+        yAxis = d3.axisLeft().scale(yScale);
+
+    //don't want dots overlapping axis, so add in buffer to data domain
+    xScale.domain(d3.extent(data, function(d) { return d[variable1]}));
+    yScale.domain(d3.extent(data, function(d) { return d[variable2]}));
 
     // x-axis
     svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", "translate(0," + (height - padding) + ")")
         .call(xAxis)
         .append("text")
         .attr("class", "label")
         .attr("x", width)
         .attr("y", -6)
         .style("text-anchor", "end")
-        .text("Timestamp");
+        .text(variable1);
 
     // y-axis
     svg.append("g")
         .attr("class", "y axis")
+        .attr("transform", "translate(" + (padding) +  ", 0)")
         .call(yAxis)
         .append("text")
         .attr("class", "label")
-        .attr("transform", "rotate(-90)")
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("CO");
+        .text(variable2);
 
     // draw dots
     svg.selectAll(".dot")
@@ -82,8 +73,11 @@ d3.csv("AndersenBoulevard.csv", function(error, data) {
         .attr("r", 3.5)
         .attr("cx", xMap)
         .attr("cy", yMap)
-        .style("fill", function(d) { return color(cValue(d));});
+        .style("fill", function (d) {
+            return color(cValue(d));
+        });
 
+    function ComputeArrayValue()
     // // draw legend
     // var legend = svg.selectAll(".legend")
     //     .data(color.domain())
@@ -105,4 +99,4 @@ d3.csv("AndersenBoulevard.csv", function(error, data) {
     //     .attr("dy", ".35em")
     //     .style("text-anchor", "end")
     //     .text(function(d) { return d;})
-});
+}
